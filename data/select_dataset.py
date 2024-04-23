fddc585cc2b58f84c0a32ad563e28ec131e8e125
Lizhe1228 <58848764+Lizhe1228@users.noreply.github.com>
2024-04-23 20:35:05 +0800

    Add files via upload

---
+++ data/select_dataset.py
@@ -0,0 +1,24 @@
+
+
+'''
+# --------------------------------------------
+# select dataset
+# --------------------------------------------
+# Kai Zhang (github: https://github.com/cszn)
+# --------------------------------------------
+'''
+
+
+def define_Dataset(dataset_opt):
+    dataset_type = dataset_opt['dataset_type'].lower()
+    if dataset_type in ['vif','med']:
+        from data.dataset_wogt import Dataset as D
+
+    else:
+        raise NotImplementedError('Dataset [{:s}] is not found.'.format(dataset_type))
+
+    dataset = D(dataset_opt)
+    print('Dataset [{:s} - {:s}] is created.'.format(dataset.__class__.__name__, dataset_opt['name']))
+    return dataset
+
+
